5aa1cdf6fd37de356750606d7f09d5c06dcdc1fc
vishnu <vishnujp12@gmail.com>
2023-10-11 17:55:26 +0530

    develop branch added

--- findfolder.py
+++ findfolder.py
@@ -1,7 +1,7 @@
 import os
 
 # Specify the directory path you want to list
-directory_path = "/master/location"
+directory_path = "/develop/location"
 
 # List files and directories in the specified path
 contents = os.listdir(directory_path)
